0f2a946373f7a26a10324aacafdc9a8476d8ca1c
FangyuanYu <fangyuan.yu18@gmail.com>
2024-02-13 13:42:50 +0800

    clean up

--- data.py
+++ data.py
@@ -14,15 +14,11 @@ turbo = dspy.OpenAI(model='gpt-3.5-turbo', max_tokens=500)
 dspy.settings.configure(lm=turbo, trace=[], temperature=0.7)
 
 
-
 # Custom Reasoning Dataset
 json_file = 'datasets/Big-Bench-Hard/bbh/causal_judgement.json'
 with open(json_file, 'r') as f:
     data = json.load(f)
-print('Keys: ', data.keys())
 examples = data['examples']
-print('Number of examples: ', len(examples))
-print('Example: ', examples[0])
 
 # Select indices for train & dev & eval || 80% train, 10% dev, 10% eval
 num_train = int(0.8 * len(examples))
@@ -35,16 +31,13 @@ for example in examples:
     answer = example['target']
     dataset.append(dspy.Example(question=question, answer=answer).with_inputs('question'))
 
-# Split the data
+# Split the data (Loaded)
 train = dataset[:num_train]
 dev = dataset[num_train:num_train+num_dev]
 eval = dataset[num_train+num_dev:]
 
+# 
 
-
-
-
-# train = [dspy.Example(question=question, answer=answer).with_inputs('question') for question, answer in train]
 
 
 
