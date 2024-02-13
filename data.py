5949d7f044284183b6f298289b3521f96b7fa578
FangyuanYu <fangyuan.yu18@gmail.com>
2024-02-13 11:34:21 +0800

    added BIG Bench Hard data

--- data.py
+++ data.py
@@ -9,3 +9,13 @@ from dspy.primitives.assertions import assert_transform_module, backtrack_handle
 
 from utils import extract_text_by_citation, correct_citation_format, has_citations, citations_check
 
+# colbertv2_wiki17_abstracts = dspy.ColBERTv2(url='http://20.102.90.50:2017/wiki17_abstracts')
+# dspy.settings.configure(rm=colbertv2_wiki17_abstracts)
+
+turbo = dspy.OpenAI(model='gpt-3.5-turbo', max_tokens=500)
+dspy.settings.configure(lm=turbo, trace=[], temperature=0.7)
+
+# Load the HotPotQA dataset
+dataset = HotPotQA(train_seed=1, train_size=300, eval_seed=2023, dev_size=300, test_size=0, keep_details=True)
+trainset = [x.with_inputs('question') for x in dataset.train]
+devset = [x.with_inputs('question') for x in dataset.dev]
